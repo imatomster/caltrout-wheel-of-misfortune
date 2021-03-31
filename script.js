a3f412e737085b87a363fda64c260b33fc59d433
Tommy <imatomster@gmail.com>
2021-03-31 14:43:53 -0700

    finished tutorial

---
+++ script.js
@@ -0,0 +1,15 @@
+function rotateFunction(){
+    // Spin in degrees
+    var min = 1024;
+    var max = 9999;
+    var deg = (Math.random()*(max-min))+min;
+    document.getElementById('box').style.transform = "rotate(" + deg + "deg)";
+    
+    var element = document.getElementById('mainbox');
+    element.classList.remove('animate');
+    
+    setTimeout(function(){
+        element.classList.add('animate');
+    }, 5000)    
+}
+
