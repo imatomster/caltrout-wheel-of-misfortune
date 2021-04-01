1b806c720c9fe89a8629b80902c0cca18a7c5ec2
imatomster <47396265+imatomster@users.noreply.github.com>
2021-03-31 20:04:56 -0700

    fixed colors and numbers

--- script.js
+++ script.js
@@ -5,16 +5,32 @@ function rotateFunction(){
     var deg = (Math.random()*(max-min))+min;
     document.getElementById('box').style.transform = "rotate(" + deg + "deg)";
     
-    var element = document.getElementById('mainbox');
-    element.classList.remove('animate');
+    var mainboxID = document.getElementById('mainbox');
+    mainboxID.classList.remove('animate');
     
     var disablingTheButton = document.getElementById('spin');
     disablingTheButton.disabled = true;
 
     setTimeout(function(){
-        element.classList.add('animate');
+        mainboxID.classList.add('animate');
         disablingTheButton.disabled = false;
+
+        console.log(getOption());
     }, 5000)    
 }
 
-
+function getOption(){
+    var elem = document.elementFromPoint(50, 50);
+    return elem;
+    // if(){
+    //     return "egg";
+    // }else if(){
+    //     return "fry";
+    // }else if(){
+    //     return "smolt";
+    // }else if(){
+    //     return "adult";
+    // }else {
+    //     return "spawner";
+    // }
+}
