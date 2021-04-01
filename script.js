40e7338e1b539bf1beef438132f47a3caffbb3b1
imatomster <47396265+imatomster@users.noreply.github.com>
2021-03-31 17:25:05 -0700

    fixed autoclicker bug

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// https://workshops.hackclub.com/spinning_wheel/
+
 function rotateFunction(){
     // Spin in degrees
     var min = 1024;
@@ -8,9 +10,13 @@ function rotateFunction(){
     var element = document.getElementById('mainbox');
     element.classList.remove('animate');
     
+    var disablingTheButton = document.getElementById('spin');
+    disablingTheButton.disabled = true;
+
     setTimeout(function(){
         element.classList.add('animate');
+        disablingTheButton.disabled = false;
     }, 5000)    
 }
 
-// https://workshops.hackclub.com/spinning_wheel/
+
